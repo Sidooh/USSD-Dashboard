f728d03a7d6755d6b4583d96a05d040c181197eb
nabcellent <nabcellent.dev@gmail.com>
2024-03-20 15:24:40 +0300

    Adds tailwind, shadcn & eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath } from "url";
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    server: {
-        port: 3000
-    },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    }
-})
+	css: {
+		postcss: {
+			plugins: [tailwind(), autoprefixer()]
+		}
+	},
+	plugins: [vue()],
+	server: {
+		port: 3000
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		}
+	}
+});
